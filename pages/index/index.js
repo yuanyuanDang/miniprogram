ba079cf9d2a267c72710a53de661e2b110feb3bc
dangyuanyuan <214827637@qq.com>
2019-10-11 20:16:28 +0800

    first commit小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,46 @@
-//index.js
-//获取应用实例
-const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+
+
+    onShareAppMessage() {
+        return {
+            title: 'swiper',
+            path: 'page/component/pages/swiper/swiper'
         }
-      })
+    },
+
+    data: {
+        background: ['demo-text-1', 'demo-text-2', 'demo-text-3','demo-text-4'],
+        background_ground:['../../images/2.jpg','../../images/1.jpg','../../images/3.jpg','../../images/4.jpg'],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 2000,
+        duration: 1000
+    },
+
+    changeIndicatorDots() {
+        this.setData({
+            indicatorDots: !this.data.indicatorDots
+        })
+    },
+
+    changeAutoplay() {
+        this.setData({
+            autoplay: !this.data.autoplay
+        })
+    },
+
+    intervalChange(e) {
+        this.setData({
+            interval: e.detail.value
+        })
+    },
+
+    durationChange(e) {
+        this.setData({
+            duration: e.detail.value
+        })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
+
